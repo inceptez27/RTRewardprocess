2a814d05100ed3425e794025b3df98dfa3e3d58a
inceptez27 <kgs.sri@gmail.com>
2022-03-11 08:26:16 +0530

    First Commit - Reward process

---
+++ src/main/scala/com/inceptez/reward/Driver.scala
@@ -0,0 +1,18 @@
+package com.inceptez.reward
+
+object Driver {
+  
+  def main(args:Array[String])=
+  {
+    //Read from Kafka -> Lookup with hbase -> calculate reward points -> Mysql
+    
+    //Step 1: Read from kafka
+    
+    //Step 2: Lookup with Hbase(Phoenix)
+    
+    //Step 3: Calculate Reward points
+    
+    //Step 4: Write into Mysql
+  }
+  
+}
